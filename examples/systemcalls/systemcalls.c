95463a939330881dceb77e9a31637aa99ac34faa
shaswathts <shaswathts@gmail.com>
2023-03-26 21:11:40 +0200

    Assignment 3 (part-1)

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -16,8 +16,46 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
+    // if (cmd == NULL) {
+    //     fprintf(stderr, "The command passed to syste() is NULL");
+    //     return EXIT_FAILURE;
+    // }
 
-    return true;
+    // int status = system(cmd);
+    // if (status != -1) { // -1 means an error with the call itself
+    //     int ret = WEXITSTATUS(status);
+    //     fprintf(stderr, "The system() call had no issue, %d, %d\n", ret, WIFEXITED(ret));
+    //     // exit(EXIT_SUCCESS);
+    // }
+
+    int status = system(cmd);
+    if (status != -1) { // -1 means an error with the call itself    
+        if (WIFEXITED(status) && !WEXITSTATUS(status)) {
+            printf("program execution successful\n"); 
+            return true;
+        }
+        
+        else if (WIFEXITED(status) && WEXITSTATUS(status)) { 
+            if (WEXITSTATUS(status) == 127) { 
+                // execv failed 
+                printf("execv failed\n"); 
+                return false;
+            } 
+            else {
+                printf("program terminated normally,"
+                " but returned a non-zero status\n");
+                return false;
+            }		 
+        } 
+        else
+            printf("program didn't terminate normally\n");			 
+    } 
+    
+    else { 
+        // waitpid() failed 
+        printf("waitpid() failed\n"); 
+    } 
+    return false;
 }
 
 /**
@@ -33,7 +71,6 @@ bool do_system(const char *cmd)
 *   fork, waitpid, or execv() command, or if a non-zero return value was returned
 *   by the command issued in @param arguments with the specified arguments.
 */
-
 bool do_exec(int count, ...)
 {
     va_list args;
@@ -59,8 +96,58 @@ bool do_exec(int count, ...)
  *
 */
 
-    va_end(args);
+    pid_t pid;
+    int status;
 
+    fflush(stdout);
+    pid = fork();
+    if (pid == -1) {
+        // fprintf(stderr, "Failed to create fork(): %s\n", strerror(errno));
+        return false; 
+    }
+
+    else if (pid == 0) {
+        // In child process. Call execv() with the path & arguments list
+        // the execv() only return if error occured & the return value is -1
+        // fprintf(stderr, "In child process, pid = %u\n", getpid()); 
+
+        // the execv() only return if error occured. 
+        // The return value is -1 
+        execv(command[0], command); 
+        exit(errno);
+    }
+
+    else {
+        // In parent process. wait for execv()/child to finish its process
+        // fprintf(stderr, "In parent process, pid = %u\n", getpid()); 
+
+        if (waitpid(pid, &status, 0) > 0) {
+            if (WIFEXITED(status) && !WEXITSTATUS(status)) {
+			    fprintf(stderr, "program execution successful\n");
+			}
+
+			else if (WIFEXITED(status) && WEXITSTATUS(status)) { 
+				if (WEXITSTATUS(status) == 127) { 
+
+					// execv failed
+					fprintf(stderr, "execv failed: %s\n", strerror(errno));
+                    return false;
+				} 
+				else {
+					fprintf(stderr, "Program terminated normally, but returned a non-zero status\n");
+                    return false;
+                }		 
+			} 
+			else {
+			    fprintf(stderr, "Program didn't terminate normally\n");
+            }
+		} 
+		else { 
+            // waitpid() failed 
+            printf("waitpid() failed\n"); 
+		} 
+    }
+    va_end(args);
     return true;
 }
 
@@ -92,7 +179,67 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
+    pid_t pid;
+    int status;
 
+    int fd = open(outputfile , O_WRONLY | O_TRUNC | O_CREAT, 0644);
+    if (fd < 0) { 
+        fprintf(stderr, "Failed to open Fd for file: %s: %s", outputfile, strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    fflush(stdout);
+    pid = fork();
+    if (pid == -1) {
+        fprintf(stderr, "Failed to create fork(): %s\n", strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    else if (pid == 0) {
+        // In child process. Call execv() with the path & arguments list
+        // the execv() only return if error occured & the return value is -1
+        // fprintf(stderr, "In child process, pid = %u\n", getpid());
+
+        // Redirect standard out to a file specified by outputfile.
+        if (dup2(fd, 1) < 0) { 
+            fprintf(stderr, "Failed to redirect standard out to a file specified by outputfile.\n"); 
+            return EXIT_FAILURE;
+        }
+        close(fd); 
+        execv(command[0], command);
+        exit(errno);
+    }
+
+    else {
+        // In parent process. wait for execv()/child to finish its process
+        // fprintf(stderr, "In parent process, pid = %u\n", getpid()); 
+
+        if (waitpid(pid, &status, 0) > 0) {
+            if (WIFEXITED(status) && !WEXITSTATUS(status)) {
+			    fprintf(stderr, "program execution successful\n");
+			}
+
+			else if (WIFEXITED(status) && WEXITSTATUS(status)) { 
+				if (WEXITSTATUS(status) == 127) { 
+
+					// execv failed
+					fprintf(stderr, "execv failed: %s\n", strerror(errno));
+                    return false;
+				} 
+				else {
+					fprintf(stderr, "Program terminated normally, but returned a non-zero status\n");
+                    return false;
+                }		 
+			} 
+			else {
+			    fprintf(stderr, "Program didn't terminate normally\n");
+            }
+		} 
+		else { 
+            // waitpid() failed 
+            printf("waitpid() failed\n"); 
+		} 
+    }
     va_end(args);
 
     return true;
--- examples/systemcalls/systemcalls.h
+++ examples/systemcalls/systemcalls.h
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdarg.h>
+#include <stdlib.h> 
+#include <errno.h> 
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <string.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 bool do_system(const char *command);
 
