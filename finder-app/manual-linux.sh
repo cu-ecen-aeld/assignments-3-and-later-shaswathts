a55e497ee32ff5f75fee2fdcb03aae78e9f389e2
shaswathts <shaswathts@gmail.com>
2023-03-31 19:29:20 +0200

    Assignment 3 (Part 2)

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -35,9 +35,67 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    # Step-1 Target Clean
+    echo "make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper"
+    make ARCH=${ARCH} \
+         CROSS_COMPILE=${CROSS_COMPILE} \
+         mrproper
+    if [ $? -eq 0 ]; then
+        echo "Clean build with flag mrproper"
+    else
+        echo "failed: Target Clean with flag mrproper"
+        exit 1
+    fi
+
+    # Step-2 Target defconfig (Configure for “virt” arm dev board to simulate in QEMU)
+    echo "make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig"
+    make ARCH=${ARCH} \
+         CROSS_COMPILE=${CROSS_COMPILE} \
+         defconfig
+    if [ $? -eq 0 ]; then
+        echo "Configure for “virt” arm dev board to simulate in QEMU"
+    else
+        echo "failed: Target defconfig"
+        exit 1
+    fi
+
+    # Step-3 Target vmlinux (Build a kernel image for booting with QEMU)
+    echo "make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} vmlinux"
+    make -j4 ARCH=${ARCH} \
+         CROSS_COMPILE=${CROSS_COMPILE} \
+         all
+    if [ $? -eq 0 ]; then
+        echo "Build kernel image for booting with QEMU"
+    else
+        echo "failed: Target vmlinux"
+        exit 1
+    fi
+
+    # Step-4 Target modules (Build any kernel modules)
+    make ARCH=${ARCH} \
+         CROSS_COMPILE=${CROSS_COMPILE} \
+         modules
+    if [ $? -eq 0 ]; then
+        echo "Build any kernel modules"
+    else
+        echo "failed: Target modules"
+        exit 1
+    fi
+
+    # Step-5 Target devicetree (Build the devicetree)
+    make ARCH=${ARCH} \
+         CROSS_COMPILE=${CROSS_COMPILE} \
+         dtbs
+    if [ $? -eq 0 ]; then
+        echo "Build the devicetree"
+    else
+        echo "failed: Target devicetree"
+        exit 1
+    fi
 fi
 
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -48,6 +106,21 @@ then
 fi
 
 # TODO: Create necessary base directories
+if [ ! -d "$OUTDIR/rootfs" ]; then
+    mkdir -p $OUTDIR/rootfs
+fi
+
+if [ -d "$OUTDIR/rootfs" ]; then
+    cd "$OUTDIR/rootfs"
+    # Create filesystem structure only if the dir rootfs does not exist.
+	mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+    mkdir -p usr/bin usr/lib usr/sbin
+    mkdir -p var/log
+    mkdir -p home/conf
+else
+    echo "${OUTDIR}/rootfs: does not exist to create filesystem structure"
+    exit 1;
+fi
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -61,20 +134,51 @@ else
 fi
 
 # TODO: Make and install busybox
+# The BusyBox build process is similar to the Linux kernel build:
+make defconfig 
+
+make ARCH=${ARCH} \
+     CROSS_COMPILE=${CROSS_COMPILE}
+
+make CONFIG_PREFIX=${OUTDIR}/rootfs \
+     ARCH=${ARCH} \
+     CROSS_COMPILE=${CROSS_COMPILE} \
+     install
 
 echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "program interpreter"
+${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+cp $CC_TOOLCHAIN_SYS/libc/lib/* ${OUTDIR}/rootfs/lib/
+cp $CC_TOOLCHAIN_SYS/libc/lib64/* ${OUTDIR}/rootfs/lib64/
 
 # TODO: Make device nodes
+sudo mknod ${OUTDIR}/rootfs/dev/null c 1 3
+sudo mknod ${OUTDIR}/rootfs/dev/console c 5 1
 
 # TODO: Clean and build the writer utility
+cd ~/work/assignment-1-shaswathts/finder-app/
+pwd
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE}
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+pwd
+cp ~/work/assignment-1-shaswathts/finder-app/conf/* ${OUTDIR}/rootfs/home/conf
+cp ~/work/assignment-1-shaswathts/finder-app/finder.sh ${OUTDIR}/rootfs/home/
+cp ~/work/assignment-1-shaswathts/finder-app/finder-test.sh ${OUTDIR}/rootfs/home/
+cp ~/work/assignment-1-shaswathts/finder-app/writer ${OUTDIR}/rootfs/home/ 
+cp ~/work/assignment-1-shaswathts/finder-app/autorun-qemu.sh ${OUTDIR}/rootfs/home/
+cp ~/work/assignment-1-shaswathts/finder-app/start-qemu-app.sh ${OUTDIR}/rootfs/home/
 
 # TODO: Chown the root directory
+cd ${OUTDIR}/rootfs/
+sudo chown -R root:root *
 
 # TODO: Create initramfs.cpio.gz
+cd ${OUTDIR}/rootfs/
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+cd ${OUTDIR}
+gzip -f initramfs.cpio
